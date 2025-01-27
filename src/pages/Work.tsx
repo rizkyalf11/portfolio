6a5de7fea9638b403a9350dffa7c561060c97648
IkkYa <akunikky11@gmail.com>
2025-01-27 08:53:35 +0700

    update

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -3,7 +3,24 @@ import MyContainer from "@/components/ui/MyContainer";
 export default function Work() {
   return (
     <MyContainer title="Work">
-      <div className="h-full w-full bg-red-200"></div>
+      <div className="grid h-full w-full grid-cols-12 gap-7">
+        <div className="col-span-6 grid grid-cols-12 rounded-base border-2 border-border bg-[#DAF5F0] shadow-light dark:border-darkBorder dark:shadow-dark">
+          <div className="col-span-12 flex gap-1 border-b-2 border-border px-2 py-2 dark:border-darkBorder">
+            <div className="aspect-square h-[15px] rounded-full border border-border bg-[#FF6060] dark:border-darkBorder"></div>
+            <div className="aspect-square h-[15px] rounded-full border border-border bg-[#FFF159] dark:border-darkBorder"></div>
+            <div className="aspect-square h-[15px] rounded-full border border-border bg-[#85FF43] dark:border-darkBorder"></div>
+          </div>
+          <div className="col-span-12 h-[200px]"></div>
+        </div>
+        <div className="col-span-6 grid grid-cols-12 rounded-base border-2 border-border bg-[#DAF5F0] shadow-light dark:border-darkBorder dark:shadow-dark">
+          <div className="col-span-12 flex gap-1 border-b-2 border-border px-2 py-2 dark:border-darkBorder">
+            <div className="aspect-square h-[15px] rounded-full border border-border bg-[#FF6060] dark:border-darkBorder"></div>
+            <div className="aspect-square h-[15px] rounded-full border border-border bg-[#FFF159] dark:border-darkBorder"></div>
+            <div className="aspect-square h-[15px] rounded-full border border-border bg-[#85FF43] dark:border-darkBorder"></div>
+          </div>
+          <div className="col-span-12 h-[200px]"></div>
+        </div>
+      </div>
     </MyContainer>
   );
 }
